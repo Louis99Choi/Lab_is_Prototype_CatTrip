fbd269b8917ce4ddda189b4af69d2a3e028c5426
Louis99Choi <73094206+Louis99Choi@users.noreply.github.com>
2021-02-06 23:30:26 +0900

    Add CountDown

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float CurrentTime;
+    public Text GameTimeText;
 
     // Update is called once per frame
     void Update()
     {
+        if (CurrentTime > 0.0f)
+        {
+            CurrentTime -= Time.deltaTime;
+            GameTimeText.text = "" + (int)CurrentTime;
+        }
+
+        else/* if (CurrentTime < 0.0f)*/
+        {
+            GameTimeText.text = "게임종료";
+            Time.timeScale = 0.0f;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("CountDown");
+                Time.timeScale = 1.0f;
+            }
+            else
+                return;
+
+        }
         
     }
 }
