93d47dd06857bd88900126204eee94dd5dce5e24
Moc-JungChan <blax1004@ahou.ac.kr>
2021-02-12 22:58:20 +0900

    Change Scene Effect

---
+++ Assets/Script/Maps/FirstMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstMap : MonoBehaviour
+{
+    public GameObject Background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Background.SetActive(false);
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        Background.SetActive(true);
+    }
+}
